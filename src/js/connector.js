9ae639ee5b24970570dda8bb170b29f2d55570b3
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-07 06:47:42 +0200

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,11 +2,26 @@ window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
         console.log("------------------------------");
         return t.card("coordinates").then((card) => {
-            return [
-                {
-                    name: "Test!!",
-                },
-            ];
+            console.log(card);
+            if (card.coordinates) {
+                const { latitude, longitude } = card.coordinates;
+                //28c85c86a948ee6429cc6013996d70ac
+                fetch(
+                    `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=%%API_KEY%%`
+                )
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((weatherData) => {
+                        return [
+                            {
+                                text: weatherData.main.temp.toString(),
+                                text: weatherData.wind.speed.toString(),
+                            },
+                        ];
+                    });
+            }
+            return [];
         });
     },
 });
