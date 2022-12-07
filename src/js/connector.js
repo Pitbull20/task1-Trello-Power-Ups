ba35f3d8eba1cf844c98eb6ea1e9e07b3218d3c6
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-07 09:11:00 +0200

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -13,13 +13,14 @@ window.TrelloPowerUp.initialize({
                         console.log(weatherData);
                         return [
                             {
-                                icon: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/samsung/349/thermometer_1f321-fe0f.png",
                                 text:
+                                    "🌡" +
                                     weatherData.hourly.temperature_2m[0].toString() +
                                     weatherData.hourly_units.temperature_2m.toString(),
                             },
                             {
                                 text:
+                                    "💨" +
                                     weatherData.hourly.windspeed_10m[0].toString() +
                                     weatherData.hourly_units.windspeed_10m.toString(),
                             },
