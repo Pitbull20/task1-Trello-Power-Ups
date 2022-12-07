0c57540ef6897d11ace6fe60ae3fa7397ba03a28
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-07 07:44:29 +0200

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,40 +4,26 @@ window.TrelloPowerUp.initialize({
             console.log(card);
             if (card.coordinates) {
                 const { latitude, longitude } = card.coordinates;
-                // //28c85c86a948ee6429cc6013996d70ac
-                // fetch(
-                //     `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=%%API_KEY%%`
-                // )
-                //     .then((response) => {
-                //         return response.json();
-                //     })
-                //     .then((weatherData) => {
-                //         console.log(weatherData);
-                //         return [
-                //             {
-                //                 text: weatherData.wind.speed.toString(),
-                //             },
-                //         ];
-                //     })
-                //     .catch((e) => {
-                //         console.log(e);
-                //     });
-                return [
-                    {
-                        icon: "../image/219243-200.png",
-                        text: latitude.toString(),
-                    },
-                    {
-                        text: longitude.toString(),
-                    },
-                ];
+                //28c85c86a948ee6429cc6013996d70ac
+                fetch(
+                    `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=%%APP_ID%%`
+                )
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((weatherData) => {
+                        console.log(weatherData);
+                        return [
+                            {
+                                text: weatherData.wind.speed.toString(),
+                            },
+                        ];
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    });
             }
             return [];
-        });
-    },
-    "card-badges": (t, opts) => {
-        return t.get("name").then((name) => {
-            console.log(name);
         });
     },
 });
