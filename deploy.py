3f0a5f4d6e14b6d39c81f441f6e3cad51c6c25a2
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-07 15:16:38 +0200

    update

--- deploy.py
+++ deploy.py
@@ -1,8 +1,14 @@
 import os
+from datetime import datetime
+
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
 
 os.system("git add .")
 print("add files-----------> OK")
 os.system("git commit -m {}".format("update"))
 print("commit-----------> OK")
 os.system("git push")
-print("Push-----------> DONE")
+print("Push-----------> DONE in {}".format(current_time))
