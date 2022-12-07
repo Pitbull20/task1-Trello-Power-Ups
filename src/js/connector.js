ac785b5d548f2dddb3520c2d9fdb083994b251e1
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-07 08:01:43 +0200

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -5,7 +5,7 @@ window.TrelloPowerUp.initialize({
             if (card.coordinates) {
                 const { latitude, longitude } = card.coordinates;
                 fetch(
-                    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Ternopil&aqi=no`
+                    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`
                 )
                     .then((response) => {
                         return response.json();
