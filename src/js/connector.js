94a42f8d6c9b2db4953a67cd39d145db29b392ed
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-07 07:25:55 +0200

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,24 +4,30 @@ window.TrelloPowerUp.initialize({
             console.log(card);
             if (card.coordinates) {
                 const { latitude, longitude } = card.coordinates;
-                //28c85c86a948ee6429cc6013996d70ac
-                fetch(
-                    `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=%%API_KEY%%`
-                )
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((weatherData) => {
-                        console.log(weatherData);
-                        return [
-                            {
-                                text: weatherData.wind.speed.toString(),
-                            },
-                        ];
-                    })
-                    .catch((e) => {
-                        console.log(e);
-                    });
+                // //28c85c86a948ee6429cc6013996d70ac
+                // fetch(
+                //     `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=%%API_KEY%%`
+                // )
+                //     .then((response) => {
+                //         return response.json();
+                //     })
+                //     .then((weatherData) => {
+                //         console.log(weatherData);
+                //         return [
+                //             {
+                //                 text: weatherData.wind.speed.toString(),
+                //             },
+                //         ];
+                //     })
+                //     .catch((e) => {
+                //         console.log(e);
+                //     });
+                return [
+                    {
+                        text: "100",
+                        text: "1500"
+                    },
+                ];
             }
             return [];
         });
