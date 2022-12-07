ac5725a44d832438bc0fc578f96bf28d27d8b8bb
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-07 05:53:06 +0200

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,9 +2,15 @@ window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
         console.log("------------------------------");
         return t.card("coordinates").then((card) => {
-            console.log(card.name);
-            console.log(card.url);
-            return [];
+            if (card.coordinates) {
+                return [
+                    {
+                        text: card.coordinates.latitude,
+                    },
+                ];
+            } else {
+                return [];
+            }
         });
     },
 });
