17324e37a3401d4bcc11ff10933dbebd4cb6d002
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-07 09:23:31 +0200

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -34,4 +34,41 @@ window.TrelloPowerUp.initialize({
             return [];
         });
     },
+    "card-detail-badges": (t) => {
+        return t.card("coordinates").then((card) => {
+            if (card.coordinates) {
+                const { latitude, longitude } = card.coordinates;
+                return fetch(
+                    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,windspeed_10m`
+                )
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((weatherData) => {
+                        console.log(weatherData);
+                        return [
+                            {
+                                title: "Temperature",
+                                text:
+                                    "🌡 " +
+                                    weatherData.hourly.temperature_2m[0].toString() +
+                                    weatherData.hourly_units.temperature_2m.toString(),
+                            },
+                            {
+                                title: "Wind",
+                                text:
+                                    "💨 " +
+                                    weatherData.hourly.windspeed_10m[0].toString() +
+                                    weatherData.hourly_units.windspeed_10m.toString(),
+                            },
+                        ];
+                    })
+                    .catch((e) => {
+                        console.log(false);
+                        console.log(e);
+                    });
+            }
+            return [];
+        });
+    },
 });
