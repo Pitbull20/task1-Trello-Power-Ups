ad587cbcbcc773d740624dcf4a6c175abbb344de
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-10 07:54:45 +0200

    update

--- src/js/settings.js
+++ src/js/settings.js
@@ -8,12 +8,24 @@ const defaultUnit = (locale) => {
 };
 
 const metricBtn = document.querySelector("#metric");
-const imperialBtn = document.querySelector("#imperial")
+const imperialBtn = document.querySelector("#imperial");
+
+document
+    .querySelectorAll("input[type=radio][name=units]")
+    .forEach((radioBtn) => {
+        radioBtn.addEventListener("change", (e) => {
+            t.set("member", "private", "units", e.target.value);
+        });
+    });
 
 t.render(() => {
     return t
         .get("member", "private", "units", defaultUnit(t.getContext().locale))
-        .then(unitPreference => {
-
+        .then((unitPreference) => {
+            if (unitPreference === "metric") {
+                metricBtn.value = true;
+            } else {
+                imperialBtn.value = true;
+            }
         });
 });
